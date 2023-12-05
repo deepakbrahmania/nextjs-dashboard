9ebe283f2c360781454cdeaf4eaa668cf953b10d
Deepak Brahmania <deepakbrahmania@Sunandinis-MacBook-Air.local>
2023-12-06 04:35:23 +0530

    fix: added alt tag for img component

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt="invoice image"
+                        alt={`invoice image for ${invoice.name}`}
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt={`invoice image for ${invoice.name}`}
                       />
                       <p>{invoice.name}</p>
                     </div>
