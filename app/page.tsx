d8505123df330d3fd61ac2ec8239f66a5bc12764
Deepak Brahmania <deepakbrahmania@Sunandinis-MacBook-Air.local>
2023-12-06 04:28:43 +0530

    fix: fonts filename updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
-import { lusitana } from '@/app/ui/font';
+import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
